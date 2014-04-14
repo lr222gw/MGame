c26957971796a258be2c5454808f535eadf8b2f5
lr222gw <lr222gw@student.lnu.se>
2014-04-14 14:01:02 +0200

    Gjort klart Vision+Kravspec

---
+++ Proto/motor.js
@@ -0,0 +1,75 @@
+var init = function(){
+	
+	
+};
+var Machines = {
+	
+	generateCards : function(){
+		//Init all Cards
+	},
+	
+	generatePlayers : function(){
+		
+		//Init all Actor
+		
+		//Pick random murderur
+		
+		//Pick murders Motive
+		
+		//Get attribut for Murderur
+		
+		//Get Attributes for all actors except from murderur
+	},
+	
+	PickMurderMotive : function(){
+		
+		//Select random Motive
+		
+		//Select Cards Based Of Motive
+		
+	}
+	
+	
+	
+};
+
+var Classes = {
+	GameCard : function(){
+		this.name = "";
+		this.type = "";
+		this.motive = ""; // Om kortet är motivbaserat så kan man ange det här
+		this.content = "";
+		this.isMurder = false;
+		this.isLie	= false;	
+	},
+	
+	Actor : function(){
+		this.name = "";
+		this.isMurder = false;	
+		this.ID = 0;
+		this.Secret = null;
+		this.Other = null;
+		this.Intress = null;
+		this.Relation = null;
+		this.StateOfMind = null;
+	},
+	
+	GameCardType : function(){
+		
+		var h ;
+		
+		var CardEnum = {
+			"Secret" : 0,
+			"Other" : 1,
+			"Intress" : 2,
+			"Relationship" : 3			
+		};
+	}
+};
+
+
+
+
+window.onload = function(){
+	init();	
+};
